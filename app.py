134a482ea19fc0c90e7d7abc81f7f828368b3b70
Anuj kumar <theanujsinha@gmail.com>
2025-01-31 11:46:03 +0530

    commit

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import joblib
 import numpy as np
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 # page tittle and page icon
